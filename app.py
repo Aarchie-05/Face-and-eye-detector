ce00fed6041c467c85ef09497fa654042d089148
Aarchie <aarchie.girdhar10@gmail.com>
2021-12-09 01:55:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,5 +36,12 @@ def index():
 def video_feed():
     return Response(generate_frames() , mimetype='multipart/x-mixed-replace; boundary=frame')
 
+import os 
+from flask import send_from_directory     
+
+@app.route('/favicon.ico') 
+def favicon(): 
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 if __name__ == "__main__":
     app.run(debug=True)
